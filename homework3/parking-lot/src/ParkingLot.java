ed69155c9a224180f9be95b16a66312bb48b2921
dujuanxian <jdu.xian@gmail.com>
2015-12-10 10:00:10 +0800

    could not park when parking lot is full

--- homework3/parking-lot/src/ParkingLot.java
+++ homework3/parking-lot/src/ParkingLot.java
@@ -2,13 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingLot {
+    private final int space;
     private List<Parking> parkings = new ArrayList<>();
 
-    public ParkingLot() {}
+    public ParkingLot(int space) {
+        this.space = space;
+    }
 
     public String parkCar(String carId) {
-        Parking parking = new Parking(parkings.size(), carId);
-        this.parkings.add(parking);
-        return parking.getVoucher();
+        if (parkings.size() < space) {
+            Parking parking = new Parking(parkings.size(), carId);
+            this.parkings.add(parking);
+            return parking.getVoucher();
+        }
+        return null;
     }
 }
--- homework3/parking-lot/test/ParkingLotTest.java
+++ homework3/parking-lot/test/ParkingLotTest.java
@@ -2,12 +2,21 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
 
 public class ParkingLotTest {
     @Test
     public void shouldParkCarWhenParkingLotHasSpace() throws Exception {
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(10);
         String voucher = parkingLot.parkCar("AAA11");
         assertThat(voucher, is("0-AAA11"));
+    }
+
+    @Test
+    public void shouldNotParkCarWhenParkingLotHasNoSpace() throws Exception {
+        ParkingLot parkingLot = new ParkingLot(1);
+        parkingLot.parkCar("AAA11");
+        String voucher = parkingLot.parkCar("AAA12");
+        assertThat(voucher, isEmptyOrNullString());
     }
 }
