ec6512caa80fe042445d2a2c707eeeeb53e3bf69
dujuanxian <jdu.xian@gmail.com>
2015-12-15 01:28:39 +0800

    parking manger park cars by the multiple parking boys

--- homework3/parking-lot/src/ParkingManager.java
+++ homework3/parking-lot/src/ParkingManager.java
@@ -1,11 +1,23 @@
-public class ParkingManager {
-    private final ParkingBoy parkingBoy;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public ParkingManager(ParkingBoy parkingBoy) {
-        this.parkingBoy = parkingBoy;
+public class ParkingManager {
+    private final List<ParkingBoy> parkingBoys;
+
+    public ParkingManager(ArrayList<ParkingBoy> parkingBoys) {
+        this.parkingBoys = parkingBoys;
     }
 
     public Parking parkCar(String carId) {
-        return this.parkingBoy.parkCar(carId);
+        return getAvailableParkingBoys().get(0).parkCar(carId);
     }
+
+    protected List<ParkingBoy> getAvailableParkingBoys() {
+        return this.parkingBoys
+                .stream()
+                .filter((parkingBoy) -> !parkingBoy.getAvailableParkingLots().isEmpty())
+                .collect(Collectors.toList());
+    }
+
 }
--- homework3/parking-lot/test/ParkingManagerTest.java
+++ homework3/parking-lot/test/ParkingManagerTest.java
@@ -8,17 +8,39 @@ import static org.hamcrest.Matchers.is;
 
 public class ParkingManagerTest {
     @Test
-    public void shouldParkingManagerParkCarByParkingLotBoy() {
+    public void shouldParkingManagerParkCarsByParkingLotBoy() {
         ArrayList<ParkingLot> parkingLots = new ArrayList<>(Arrays.asList(
                 new ParkingLot(1, "lot-1"),
                 new ParkingLot(5, "lot-2")));
 
         ParkingManager parkingManager = new ParkingManager(
-                new JuniorParkingBoy(parkingLots));
+                new ArrayList<>(Arrays.asList(
+                    new JuniorParkingBoy(parkingLots))));
         Parking parking1 = parkingManager.parkCar("AAA11");
         Parking parking2 = parkingManager.parkCar("AAA22");
         
         assertThat(parking1, is(new Parking("AAA11", "0", "lot-1")));
         assertThat(parking2, is(new Parking("AAA22", "0", "lot-2")));
+    }
+
+    @Test
+    public void shouldParkingManagerParkCarsByMultipleParkingLotBoys() {
+        ArrayList<ParkingLot> parkingLots1 = new ArrayList<>(Arrays.asList(
+                new ParkingLot(1, "lot-1")));
+        ArrayList<ParkingLot> parkingLots2 = new ArrayList<>(Arrays.asList(
+                new ParkingLot(1, "lot-2"),
+                new ParkingLot(5, "lot-3")));
+
+        ParkingManager parkingManager = new ParkingManager(
+                new ArrayList<>(Arrays.asList(
+                    new JuniorParkingBoy(parkingLots1),
+                    new SeniorParkingBoy(parkingLots2))));
+        Parking parking1 = parkingManager.parkCar("AAA11");
+        Parking parking2 = parkingManager.parkCar("AAA22");
+        Parking parking3 = parkingManager.parkCar("AAA33");
+        
+        assertThat(parking1, is(new Parking("AAA11", "0", "lot-1")));
+        assertThat(parking2, is(new Parking("AAA22", "0", "lot-3")));
+        assertThat(parking3, is(new Parking("AAA33", "1", "lot-3")));
     }
 }
