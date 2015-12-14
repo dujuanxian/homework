685b0aa54148b29f15bcc94d82ead8bb8092d9b4
dujuanxian <jdu.xian@gmail.com>
2015-12-14 18:48:14 +0800

    extract parkingLotBoy to abstract class

--- homework3/parking-lot/src/ParkingLotBoy.java
+++ homework3/parking-lot/src/ParkingLotBoy.java
@@ -1,20 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class ParkingLotBoy {
-    private final ArrayList<ParkingLot> parkingLots;
+public abstract class ParkingLotBoy {
+    protected final ArrayList<ParkingLot> parkingLots;
 
-    public ParkingLotBoy(ArrayList<ParkingLot> parkingLots) {
+    protected ParkingLotBoy(ArrayList<ParkingLot> parkingLots) {
         this.parkingLots = parkingLots;
     }
 
+    protected abstract List<ParkingLot> getAvailableParkingLots();
+
     public Parking parkCar(String carId) {
-        List<ParkingLot> availableParkingLots = 
-                this.parkingLots
-                .stream()
-                .filter(ParkingLot::hasSpace)
-                .collect(Collectors.toList());
-        return availableParkingLots.isEmpty() ? null : availableParkingLots.get(0).parkCar(carId);
+        return getAvailableParkingLots().isEmpty() ? null : getAvailableParkingLots().get(0).parkCar(carId);
     }
 }
